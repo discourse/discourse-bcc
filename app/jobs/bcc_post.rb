ebc42ee6c0e7fff0d1a05f6fb6c972ecd34b3f20
Qursch <66182320+Qursch@users.noreply.github.com>
2021-04-19 12:37:29 -0400

    Change Syntax & Add Name Personalization (#9)
    * Changed syntax
    
    * Change Syntax and Add Name
    
    * Cleaning Up Work

--- app/jobs/bcc_post.rb
+++ app/jobs/bcc_post.rb
@@ -22,8 +22,12 @@ class ::Jobs::BccPost < ::Jobs::Base
   def send_to(targets, targets_key, params, sender)
     targets.each do |target|
       temp_params = params
-      temp_params["raw"] = temp_params["raw"].gsub(/{username}/i, target)
-      temp_params["raw"] = temp_params["raw"].gsub(/{@username}/i, "@" + target)
+      temp_params["raw"] = temp_params["raw"].gsub(/%{username}/i, target)
+      temp_params["raw"] = temp_params["raw"].gsub(/%{@username}/i, "@" + target)
+      user = User.find_by_username_or_email(target)
+      if !user.nil? then
+        temp_params["raw"] = temp_params["raw"].gsub(/%{name}/i, user.name)
+      end
       creator = PostCreator.new(sender, temp_params.merge(Hash[targets_key, target]))
       creator.create
     end
