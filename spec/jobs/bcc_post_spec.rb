74442fdec4d21747062daea5cf7e478209a76cc8
Qursch <qursch@outlook.com>
2021-05-10 12:45:00 -0400

    Fix personalization (#11)
    * Fix personalization
    
    * Add first and last name personalization
    
    * Add and change tests
    
    Added tests for first and last name personalization, as well as changed all related tests to check for a message to the second user, which assures that not all users are receiving the same message.
    
    * Remove first and last name personalization
    
    * Remove hash cloning
    
    * Fix spacing
    
    * Remove spaces
    
    Didn't realize what the check was looking for originally.

--- spec/jobs/bcc_post_spec.rb
+++ spec/jobs/bcc_post_spec.rb
@@ -59,23 +59,24 @@ describe ::Jobs::BccPost do
 
     it 'works with standard personalization' do
       ::Jobs::BccPost.new.execute(user_id: sender.id, create_params: create_params.merge("raw": "this is the content I want to send to %{username}", target_emails: 'test@test.com'))
-      post = Post.find_by(raw: "this is the content I want to send to #{user0.username}")
+      post = Post.find_by(raw: "this is the content I want to send to #{user1.username}")
 
       expect(post).to_not be_nil
     end
 
     it 'works with mention personalization' do
       ::Jobs::BccPost.new.execute(user_id: sender.id, create_params: create_params.merge("raw": "this is the content I want to send to %{@username}", target_emails: 'test@test.com'))
-      post = Post.find_by(raw: "this is the content I want to send to @#{user0.username}")
+      post = Post.find_by(raw: "this is the content I want to send to @#{user1.username}")
 
       expect(post).to_not be_nil
     end
 
     it 'works with name personalization' do
       ::Jobs::BccPost.new.execute(user_id: sender.id, create_params: create_params.merge("raw": "this is the content I want to send to %{name}", target_emails: 'test@test.com'))
-      post = Post.find_by(raw: "this is the content I want to send to #{user0.name}")
+      post = Post.find_by(raw: "this is the content I want to send to #{user1.name}")
 
       expect(post).to_not be_nil
     end
+
   end
 end
