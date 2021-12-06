discourse/discourse-bcc

commit 668bd99be87ee9cb4d7413c101eae9538e87262a

DEV: Reduce Server Load (#23)
* DEV: Reduce Server Load

This commit attempts to smooth out server load when sending PMs/emails
to hundreds or even thousands of users by batching the creation of
sidekiq jobs into groups of 20 instead of using a single sidekiq job to
do all of the sending.

Before when a single sidekiq job was doing all the work this would
hammer a single CPU/worker. Now that multiple sidekiq jobs are created
for sending out messages we can take advantage of using multiple workers
to share the load.

- Switched to using a lower priority queue
- Removed splitting up the usernames and emails from the sidekiq job
- Changed the sidekiq job args so that it now accepts a `targets` and a
`targets_key` param
- The bcc controller endpoint now handles splitting up the users in
batches and creating single sidekiq job per patch
- Cleaned up all the tests to match the new format

* Add test for batching

* create a method to encapsulate the batching loop

* Update plugin.rb with each_slice

Use each_slice to do the batching

Co-authored-by: Régis Hanol <regis@hanol.fr>

* fix rubocop alignment issue and use BCC spelling

Co-authored-by: Régis Hanol <regis@hanol.fr>

plugin.rb
@@ -11,6 +11,10 @@ enabled_site_setting :bcc_enabled
 after_initialize do
   require_relative "app/jobs/bcc_post"
 
+  module ::DiscourseBCC
+    BATCH_SIZE = 20
+  end
+
   Discourse::Application.routes.append do
     post '/posts/bcc' => 'posts#bcc', constraints: StaffConstraint.new
   end
@@ -27,6 +31,18 @@ after_initialize do
           status: result.success? ? 200 : 422
         )
       end
+
+      def batch_targets(targets, targets_key)
+        targets.each_slice(DiscourseBCC::BATCH_SIZE) do |t|
+          Jobs.enqueue(
+            :bcc_post,
+            user_id: current_user.id,
+            create_params: @manager_params,
+            targets_key: targets_key,
+            targets: t
+          )
+        end
+      end
     end
   end
 
@@ -40,7 +56,7 @@ after_initialize do
     usernames = Set.new((@manager_params[:target_usernames] || '').split(','))
 
     # Expand any groups
-    group_names = (@manager_params.delete(:target_group_names) || '').split(',')
+    group_names = Set.new((@manager_params[:target_group_names] || '').split(','))
 
     Group.where('lower(name) in (?)', group_names).includes(group_users: :user).each do |g|
       g.group_users.each do |gu|
@@ -54,8 +70,6 @@ after_initialize do
       return render_bcc(status: false) { |result| result.add_error(I18n.t("bcc.too_few_users")) }
     end
 
-    @manager_params[:target_usernames] = usernames.to_a.join(',')
-
     validator = PostValidator.new
     post = Post.new(raw: @manager_params[:raw], user: current_user)
     validator.validate(post)
@@ -64,7 +78,12 @@ after_initialize do
       return render_bcc(status: false) { |result| result.add_error(post.errors[:raw]) }
     end
 
-    Jobs.enqueue(:bcc_post, user_id: current_user.id, create_params: @manager_params)
+    @manager_params.except(:target_users, :target_group_names, :target_emails)
+
+    # Queue up jobs in batches so that when sending hundreds or thousands of emails we
+    # can take advantage of multiple workers
+    batch_targets(usernames, 'target_usernames')
+    batch_targets(emails, 'target_emails')
 
     return render_bcc(status: true) do |result|
       result.route_to = "/u/#{current_user.username_lower}/messages/sent"

spec/requests/post_controller_spec.rb
@@ -96,7 +96,7 @@ describe PostsController do
         expect(response.code).to eq('200')
         job = Jobs::BccPost.jobs[0]
         expect(job).to be_present
-        usernames = job['args'].first['create_params']['target_usernames'].split(',')
+        usernames = job['args'].first['targets']
         expect(usernames).to match_array([@user0.username, @user1.username, @user2.username])
       end
 
@@ -108,8 +108,36 @@ describe PostsController do
         expect(response.code).to eq('200')
         job = Jobs::BccPost.jobs[0]
         expect(job).to be_present
-        usernames = job['args'].first['create_params']['target_usernames'].split(',')
+        usernames = job['args'].first['targets']
         expect(usernames).to match_array([@user0.username, @user1.username])
+      end
+    end
+
+    describe 'batching jobs' do
+      before do
+        @group = Fabricate(:group, messageable_level: Group::ALIAS_LEVELS[:everyone])
+        i = 0
+        while i < DiscourseBCC::BATCH_SIZE + 5
+          user = Fabricate(:user)
+          GroupUser.create(group: @group, user: user)
+          i += 1
+        end
+      end
+
+      it 'batches users based on batch size' do
+        post '/posts/bcc.json', params: create_params.merge(
+          target_recipients: "#{@group.name}"
+        )
+
+        expect(response.code).to eq('200')
+        job_batch_1 = Jobs::BccPost.jobs[0]
+        expect(job_batch_1).to be_present
+        job_batch_2 = Jobs::BccPost.jobs[1]
+        expect(job_batch_2).to be_present
+        usernames_batch_1 = job_batch_1['args'].first['targets']
+        usernames_batch_2 = job_batch_2['args'].first['targets']
+        expect(usernames_batch_1.size).to eq(DiscourseBCC::BATCH_SIZE)
+        expect(usernames_batch_2.size).to eq(5)
       end
     end
   end

app/jobs/bcc_post.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
 class ::Jobs::BccPost < ::Jobs::Base
+
+  sidekiq_options queue: 'low'
+
   def execute(args)
     return unless SiteSetting.bcc_enabled?
 
     sender = User.find_by(id: args[:user_id])
     return unless sender.present?
 
+    targets = args[:targets]
+    targets_key = args[:targets_key]
     create_params = args[:create_params]
     create_params[:skip_validations] = true
 
-    split_usernames = (create_params.delete(:target_usernames) || '').split(',')
-    split_emails = (create_params.delete(:target_emails) || '').split(',')
-
-    send_to(split_usernames, :target_usernames, create_params, sender)
-    send_to(split_emails, :target_emails, create_params, sender)
+    send_to(targets, targets_key, create_params, sender)
   end
 
   private

spec/jobs/bcc_post_spec.rb
@@ -6,13 +6,13 @@ describe ::Jobs::BccPost do
   fab!(:sender) { Fabricate(:moderator) }
   fab!(:user0) { Fabricate(:user) }
   fab!(:user1) { Fabricate(:user) }
+  let(:usernames) { [user0.username, user1.username] }
 
   let(:create_params) do
     HashWithIndifferentAccess.new(
       "raw" => "this is the content I want to send",
       "title" => "this is the title of the PM I want to send",
-      "archetype" => Archetype.private_message,
-      target_usernames: "#{user0.username},#{user1.username}"
+      "archetype" => Archetype.private_message
     )
   end
 
@@ -29,20 +29,42 @@ describe ::Jobs::BccPost do
 
     it "will send messages to each user" do
       topic_count = Topic.count
-      ::Jobs::BccPost.new.execute(user_id: sender.id, create_params: create_params)
+      ::Jobs::BccPost.new.execute(
+        user_id: sender.id,
+        create_params: create_params,
+        targets_key: 'target_usernames',
+        targets: [user0.username, user1.username]
+      )
       expect(Topic.count).to eq(topic_count + 2)
     end
 
     it "does not crash when user's name is empty" do
       user0.update!(name: nil)
-      expect { ::Jobs::BccPost.new.execute(user_id: sender.id, create_params: create_params) }.not_to raise_error
+      expect {
+        ::Jobs::BccPost.new.execute(
+          user_id: sender.id,
+          create_params: create_params,
+          targets_key: 'target_usernames',
+          targets: usernames)
+      }.not_to raise_error
     end
 
     it 'works when mixing emails and usernames' do
       SiteSetting.enable_staged_users = true
       topic_count = Topic.count
 
-      ::Jobs::BccPost.new.execute(user_id: sender.id, create_params: create_params.merge(target_emails: 'test@test.com'))
+      ::Jobs::BccPost.new.execute(
+        user_id: sender.id,
+        create_params: create_params,
+        targets_key: 'target_usernames',
+        targets: [user0.username, user1.username])
+
+      ::Jobs::BccPost.new.execute(
+        user_id: sender.id,
+        create_params: create_params,
+        targets_key: 'target_emails',
+        targets: ['test@test.com']
+      )
 
       expect(Topic.count).to eq(topic_count + 3)
     end
@@ -53,31 +75,50 @@ describe ::Jobs::BccPost do
 
       ::Jobs::BccPost.new.execute(
         user_id: sender.id,
-        create_params: create_params.merge(
-          target_usernames: nil,
-          target_emails: 'test@test.com,test2@test.com'
-        )
+        create_params: create_params,
+        targets_key: 'target_emails',
+        targets: ['test@test.com', 'test2@test.com']
       )
 
       expect(Topic.count).to eq(topic_count + 2)
     end
 
     it 'works with standard personalization' do
-      ::Jobs::BccPost.new.execute(user_id: sender.id, create_params: create_params.merge("raw": "this is the content I want to send to %{username}", target_emails: 'test@test.com'))
+      ::Jobs::BccPost.new.execute(
+        user_id: sender.id,
+        create_params: create_params.merge(
+          "raw": "this is the content I want to send to %{username}"
+        ),
+        targets_key: 'target_usernames',
+        targets: [user1.username])
       post = Post.find_by(raw: "this is the content I want to send to #{user1.username}")
 
       expect(post).to_not be_nil
     end
 
     it 'works with mention personalization' do
-      ::Jobs::BccPost.new.execute(user_id: sender.id, create_params: create_params.merge("raw": "this is the content I want to send to %{@username}", target_emails: 'test@test.com'))
+      ::Jobs::BccPost.new.execute(
+        user_id: sender.id,
+        create_params: create_params.merge(
+          "raw": "this is the content I want to send to %{@username}"
+        ),
+        targets_key: 'target_usernames',
+        targets: [user1.username]
+      )
       post = Post.find_by(raw: "this is the content I want to send to @#{user1.username}")
 
       expect(post).to_not be_nil
     end
 
     it 'works with name personalization' do
-      ::Jobs::BccPost.new.execute(user_id: sender.id, create_params: create_params.merge("raw": "this is the content I want to send to %{name}", target_emails: 'test@test.com'))
+      ::Jobs::BccPost.new.execute(
+        user_id: sender.id,
+        create_params: create_params.merge(
+          "raw": "this is the content I want to send to %{name}"
+        ),
+        targets_key: 'target_usernames',
+        targets: [user1.username]
+      )
       post = Post.find_by(raw: "this is the content I want to send to #{user1.name}")
 
       expect(post).to_not be_nil

commit 91ae5ace79f082fa413ec76582da51dc7e8d6523

FIX: Groups being mentioned in each message
Strip out `target_groups_names` when sending a BCC PM.

Because we are sending a single message per user we need to clean up the
create_params we are sending to the PostCreator. This is a regression
introduced in my previous commit: 668bd99be87ee9cb4d7413c101eae9538e87262a

If we leave in the `target_groups_names` param, each PM will say its
between the group and the sender instead of between the individual and
the sender.

plugin.rb
@@ -78,7 +78,7 @@ after_initialize do
       return render_bcc(status: false) { |result| result.add_error(post.errors[:raw]) }
     end
 
-    @manager_params.except(:target_users, :target_group_names, :target_emails)
+    @manager_params.except!(:target_users, :target_group_names, :target_emails)
 
     # Queue up jobs in batches so that when sending hundreds or thousands of emails we
     # can take advantage of multiple workers

spec/requests/post_controller_spec.rb
@@ -136,6 +136,10 @@ describe PostsController do
         expect(job_batch_2).to be_present
         usernames_batch_1 = job_batch_1['args'].first['targets']
         usernames_batch_2 = job_batch_2['args'].first['targets']
+        create_params = job_batch_2['args'].first['create_params']
+        expect(create_params.key?('target_group_names')).to eq(false)
+        expect(create_params.key?('target_users')).to eq(false)
+        expect(create_params.key?('target_emails')).to eq(false)
         expect(usernames_batch_1.size).to eq(DiscourseBCC::BATCH_SIZE)
         expect(usernames_batch_2.size).to eq(5)
       end